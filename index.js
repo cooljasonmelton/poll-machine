e7a48ef77e7baa4a8dfe78bf09d50dcda3515620
cooljasonmelton <cooljasonmelton>
2020-10-09 22:01:01 -0500

    added demo gif and documented things

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const run = () =>  {
+
+    // VOTE BTN ARR
     const btnArr = [...document.getElementsByClassName('option')];
+    // RESULT TEXT ARR
     const txtArr = [...document.getElementsByClassName('results')];
+    // CHART CANVAS
     let chartCvs = document.getElementById('r-chart')
 
+    // INITIAL VOTE COUNT
     let resultA = 0
     let resultB = 0
     let resultC = 0
 
+    // ADD CLICK LISTENER TO BTNS
     const clickableBtns = () => {
         return btnArr.map(btn => {
             return btn.addEventListener('click', () => updateVote(btn.id.charAt(0), newChart))
         })
     }
 
+
     const updateVote = (choice, chart) => {
+        // ADD ONE TO CHOICE
         if (choice === 'a') resultA++
         if (choice === 'b') resultB++
         if (choice === 'c') resultC++
+
+        // UPDATE RESULT TEXT
         txtArr.map(txt=> {
             let ch = txt.id.charAt(0)
             if (ch === 'a') return txt.innerHTML = resultA
@@ -24,12 +34,14 @@ const run = () =>  {
             if (ch === 'c') return txt.innerHTML = resultC
         })
 
+        // UPDATE CHART DATA
         chart.data.datasets[0].data = [resultA]
         chart.data.datasets[1].data = [resultB]
         chart.data.datasets[2].data = [resultC]
         chart.update()
     }
 
+    // CHART SPECS
     const newChart = new Chart(chartCvs, {
         type: 'bar',
         data: {
@@ -63,6 +75,7 @@ const run = () =>  {
                 display: true,
                 text: ["Results"]
             },
+            // TURN OFF ANNOYING HOVER POPUP
             tooltips: { enabled: false },
             scales: {
                 yAxes: [{
@@ -74,10 +87,6 @@ const run = () =>  {
             }
         }
     });
-
-
-
     clickableBtns()
 }
-
 run()
